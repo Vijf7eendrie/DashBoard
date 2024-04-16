92c8bd771bf75c061abfb99badf64befbfd06b26
rayan <rayanaghmer072@outlook.com>
2024-04-16 10:09:12 +0200

    nieuwstebestanden

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$db_server = "mariadb";
+$db_user = "duurzaam1";
+$db_pass = "";
+$db_name: "duurzaamhuis";
+
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+?>
---
+++ js/chart1.js
@@ -0,0 +1,24 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+            label: 'Verdiensten in Euro',
+            data: [357, 405, 431, 231, 912, 382, 2000, 918, 1124, 1231, 832, 2900],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+
+    
+});
+
